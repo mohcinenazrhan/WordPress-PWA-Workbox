801f5eac7327759c06ece75b398d23995cffe8cc
mohcinenazrhan <nazrhan.mohcine@gmail.com>
2019-06-28 20:28:45 +0100

    feat: add router to cache same origin requests js & css files

--- sw/sw.js
+++ sw/sw.js
@@ -2,6 +2,14 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox
 
 if (workbox) {
 	console.log(`Yay! Workbox is loaded 🎉`);
+
+	// Register a route to let’s add a cache fallback to our JavaScript & CSS files of same origin
+	workbox.routing.registerRoute(
+		new RegExp('\\.(js|css)'),
+		new workbox.strategies.StaleWhileRevalidate({
+			cacheName: 'static-resources'
+		})
+	);
 } else {
 	console.log(`Boo! Workbox didn't load 😬`);
 }
